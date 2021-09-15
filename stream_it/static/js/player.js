4ff54b046196b7bf25e963a77a3640a211728e08
Naval <naval.surange@students.iiit.ac.in>
2021-09-15 09:28:25 +0530

    back commit

---
+++ stream_it/static/js/player.js
@@ -0,0 +1,8 @@
+// var iframe = document.getElementById('the-iframe');
+// var style = document.createElement('style');
+// style.textContent =
+//   'body {' +
+//   '  background-color: red;' +
+//   '}' 
+// ;
+// iframe.contentDocument.head.appendChild(style);
